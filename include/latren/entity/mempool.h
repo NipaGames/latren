e14bde67968bfd9e24dc1fd970d3d572ca2d32da
NipaGames <nipagamesofficial@gmail.com>
2024-02-08 17:10:56 +0200

    will continue this

--- include/latren/entity/mempool.h
+++ include/latren/entity/mempool.h
@@ -3,23 +3,51 @@
 #include <vector>
 #include <memory>
 #include <unordered_map>
+#include <unordered_set>
+#include "component.h"
 
-typedef size_t ComponentType;
-
+typedef size_t ComponentIndex;
+struct ComponentReference;
 class IComponentMemoryPool {
-
+public:
+    virtual ComponentReference AllocNewComponent(const ComponentData& = ComponentData()) = 0;
+    virtual IComponent& GetComponent(ComponentIndex) const = 0;
+    template <typename C>
+    C& GetComponent(ComponentIndex i) const {
+        return dynamic_cast<C&>(GetComponent(i));
+    }
 };
 
-template <typename T>
+struct ComponentReference {
+    IComponentMemoryPool* pool;
+    ComponentIndex index;
+    IComponent& GetComponent() const {
+        return pool->GetComponent(index);
+    };
+    operator IComponent&() const {
+        return GetComponent();
+    }
+};
+
+template <typename C>
 class ComponentMemoryPool {
 private:
-    std::vector<T> components_;
-
+    std::vector<C> components_;
+    std::unordered_map<ComponentIndex, size_t> references_;
+    ComponentIndex currentIndex_ = 0;
+public:
+    ComponentReference AllocNewComponent(const ComponentData& data = ComponentData()) override {
+        components_.push_back(C(data));
+        references_[currentIndex_] = components_.size() - 1;
+        return { this, currentIndex_++ };
+    }
+    IComponent& GetComponent(ComponentIndex i) override const {
+        return components_.at(references_.at(i));
+    }
 };
 
 class MasterMemoryPool {
     // optimally all the pools would be stored contiguously but this is the best i can come up with
     // afaik std any doesn't store the memory itself
-    std::unordered_map<ComponentType, std::unique_ptr<IComponentMemoryPool>> componentPools_;
-
+    std::unordered_map<std::type_index, std::unique_ptr<IComponentMemoryPool>> componentPools_;
 };
